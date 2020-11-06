d1a183f7df8ab2930d9e93da786d31978da683f3
Cesar Fabiano <cesar.fabiano.hu3@outlook.com>
2020-11-05 21:28:41 -0300

    verifica_campo

---
+++ Controller/contatoController.php
@@ -0,0 +1,187 @@
+<?php
+// Inicia a Session
+session_start();
+
+// 1. Cadastrar
+if (isset($_POST['cadastrar'])) {
+    cadastrarContato();
+
+// 2. Buscar
+} elseif (isset($_POST['buscar'])) {
+    buscarContato();
+
+// 3. Editar
+} elseif (isset($_POST['editar'])) {
+    editarContato();
+
+// 4. Excluir
+} elseif (isset($_POST['excluir'])) {
+    excluirContato();
+
+// 5. Login
+} elseif (isset($_POST['login'])) {
+    efetuarLogin();
+
+// 6. Logout
+} else if (isset($_POST['logout'])) {
+    efetuarLogout();
+
+// Se não veio de nenhum clique
+} else {
+    header("Location: ../View/home.php");
+}
+
+/* FUNCTIONS */
+
+function cadastrarContato()
+{
+    // Inclui os arquivos (Model)
+    include_once "../Model/Contato.php";
+    include_once "../Model/ContatoService.php";
+
+    // Retorno do JSON (validação)
+    header('Content-Type: application/json');
+
+    // Guarda os dados informados no formulário
+    $nome  = $_POST['nome'];
+    $sobrenome  = $_POST['sobrenome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // Cria o objeto das classes Contato e ContatoService
+    $contato = new Contato();
+    $service = new ContatoService();
+
+    $contato->nome  = $nome;
+    $contato->sobrenome = $sobrenome;
+    $contato->email = $email;
+    $contato->senha = $senha;
+    
+    // Envia o objeto para efetuar o Cadastro
+    $response = $service->cadastrarContato($contato);
+    
+    // Verifica o tipo de retorno
+    if ($response['sucesso']) {
+        // Mostra a mensagem de Sucesso
+        print json_encode(array(
+            'mensagem' => $response['mensagem'],
+            'codigo' => 1));
+        exit();
+    
+    } else {
+        print json_encode(array(
+            'mensagem' => $response['mensagem'],
+            'campo' => $response['campo'],
+            'codigo' => 0));
+        exit();
+    }
+}
+
+function buscarContato()
+{
+    echo "buscarContato";
+}
+
+function editarContato()
+{
+    // Inclui os arquivos (Model)
+    include_once "../Model/Contato.php";
+    include_once "../Model/ContatoService.php";
+
+    // Retorno do JSON (validação)
+    header('Content-Type: application/json');
+
+    // Guarda os dados informados no formulário
+    $nome  = $_POST['nome'];
+    $sobrenome  = $_POST['sobrenome'];
+    $email = $_POST['email'];
+    $id = $_POST['id'];
+
+    // Cria o objeto das classes Contato e ContatoService
+    $contato = new Contato();
+    $service = new ContatoService();
+
+    $contato->nome  = $nome;
+    $contato->sobrenome = $sobrenome;
+    $contato->email = $email;
+    $contato->id = $id;
+    
+    // Envia o objeto para ser editado
+    $response = $service->editarContato($contato);
+    
+    // Verifica o tipo de retorno
+    if ($response['sucesso']) {
+        // Mostra a mensagem de Sucesso
+        print json_encode(array(
+            'mensagem' => $response['mensagem'],
+            'codigo' => 1));
+        exit();
+    
+    } else {
+        print json_encode(array(
+            'mensagem' => $response['mensagem'],
+            'campo' => $response['campo'],
+            'codigo' => 0));
+        exit();
+    }
+}
+
+function excluirContato()
+{
+    echo "excluirContato";
+}
+
+// Efetua Login no Sistema
+function efetuarLogin()
+{
+    // Inclui os arquivos (Model)
+    include_once "../Model/Contato.php";
+    include_once "../Model/ContatoService.php";
+
+    // Retorno do JSON (validação)
+    header('Content-Type: application/json');
+
+    // Guarda os dados informados no formulário
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // Cria o objeto das classes Contato e ContatoService
+    $contato = new Contato();
+    $service = new ContatoService();
+
+    $contato->email = $email;
+    $contato->senha = $senha;
+
+    // Envia o objeto para efetuar o Login
+    $response = $service->efetuarLogin($contato);
+
+    // Verifica o tipo de retorno
+    if ($response['sucesso']) {
+        $resultado = $response['resultado'];
+
+        // Guarda os dados na Session
+        $_SESSION['id']     = $resultado['id'];
+        $_SESSION['email']  = $resultado['email'];
+        $_SESSION['nome']   = $resultado['nome'];
+
+        // Mostra a mensagem de Sucesso
+        print json_encode(array(
+            'mensagem' => $response['mensagem'],
+            'codigo' => 1));
+        exit();
+    
+    // Mostra a mensagem de Erro
+    } else {
+        print json_encode(array(
+            'mensagem' => $response['mensagem'],
+            'campo' => $response['campo'],
+            'codigo' => 0));
+        exit();
+    }
+}
+
+// Efetua Logout no Sistema
+function efetuarLogout()
+{
+    session_destroy();
+}
---
+++ View/js/scripts.js
@@ -0,0 +1,167 @@
+// Cadastro de Contato
+function cadastrarContato() {
+    dados = {
+        'nome': $('#nome').val(),
+        'sobrenome': $('#sobrenome').val(),
+        'email': $('#email').val(),
+        'senha': $('#senha').val(),
+        'cadastrar': $('#cadastrar').val()
+    };
+
+    // Mostra o Loading
+    carregarModalLoading();
+
+    $.ajax({
+        url: '../Controller/contatoController.php',
+        type: 'POST',
+        data: dados,
+        success: function(data) {
+
+            setTimeout(function() {
+                esconderModalLoading();
+                $('#status').text(data.mensagem);
+
+                // Verifica o código retornado
+                if (data.codigo == 1) {
+
+
+                    $('#status').css({
+                        "color": "#49a300"
+                    });
+                    // Redireciona para o index depois do Delay
+                    setTimeout(function() {
+                        window.location.href = "cadastro.php";
+                    }, 2000); // Tempo em milissegundos
+                
+                } else {
+
+                    $(data.campo).focus();
+                    $('#status').css({
+                        "color": "#ff6f65"
+                    });
+                }
+            }, 1000);
+        }
+    });
+}
+function editarContato() {
+    dados = {
+        'nome': $('#nome').val(),
+        'sobrenome': $('#sobrenome').val(),
+        'email': $('#email').val(),
+        'id': $('#id').val(),
+        'editar': $('#editar').val()
+    };
+
+    // Mostra o Loading
+    carregarModalLoading();
+
+    $.ajax({
+        url: '../Controller/contatoController.php',
+        type: 'POST',
+        data: dados,
+        success: function(data) {
+
+            setTimeout(function() {
+                esconderModalLoading();
+                $('#status').text(data.mensagem);
+
+                // Verifica o código retornado
+                if (data.codigo == 1) {
+
+
+                    $('#status').css({
+                        "color": "#49a300"
+                    });
+                    // Redireciona para o index depois do Delay
+                    setTimeout(function() {
+                        window.location.href = "editar.php";
+                    }, 2000); // Tempo em milissegundos
+                
+                } else {
+
+                    $(data.campo).focus();
+                    $('#status').css({
+                        "color": "#ff6f65"
+                    });
+                }
+            }, 1000);
+        }
+    });
+}
+
+// Login
+function efetuarLogin() {
+    dados = {
+        'email': $('#email').val(),
+        'senha': $('#senha').val(),
+        'login': $('#login').val()
+    };
+
+    // Mostra o Loading
+    carregarModalLoading();
+
+    $.ajax({
+        url: '../Controller/contatoController.php',
+        type: 'POST',
+        data: dados,
+        success: function(data) {
+            // Mostra a mensagem
+            $('#status').text(data.mensagem);
+            
+            // Verifica o código retornado
+            if (data.codigo == 1) {
+                $('#status').css({
+                    "color": "#f1c40f"
+                });
+                // Redireciona para o index depois do Delay
+                setTimeout(function() {
+                    window.location.href = "home.php";
+                }, 2000); // Tempo em milissegundos
+            
+            } else {
+                $(data.campo).focus();
+                $('#status').css({
+                    "color": "#ff6f65"
+                });
+                setTimeout(function() {
+                    esconderModalLoading();
+                }, 2000); // Tempo em milissegundos
+            }
+        }
+    });
+}
+
+// Logout
+function efetuarLogout() {
+    dados = {
+        'logout': $('#logout').val()
+    }
+
+    // Mostra o Loading
+    carregarModalLoading();
+
+    $.ajax({
+        url: '../Controller/contatoController.php',
+        type: 'POST',
+        data: dados,
+        success: function() {
+            // Redireciona para o index depois do Delay
+            setTimeout(function() {
+                window.location.href = "home.php"
+            }, 2000); // Tempo em milissegundos
+        }
+    });
+}
+
+function carregarModalLoading() {
+    $('#modalLoading').css({
+        "display": "block"
+    });
+}
+
+function esconderModalLoading() {
+    $('#modalLoading').css({
+        "display": "none"
+    });
+}
